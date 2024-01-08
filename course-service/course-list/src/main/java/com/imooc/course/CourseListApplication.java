452787a9faee6215f1c4d6c907ffa4b63729a3c0
PQBUG <hongyujia404@gmail.com>
2024-01-08 20:19:34 +0800

    eureka

--- course-service/course-list/src/main/java/com/imooc/course/CourseListApplication.java
+++ course-service/course-list/src/main/java/com/imooc/course/CourseListApplication.java
@@ -2,7 +2,9 @@ package com.imooc.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CourseListApplication {
     public static void main(String[] args) {
